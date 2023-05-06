2c212ecabfd2aafbe54b0a3d8323f65cd5748efb
Re:Gelu <morandva@yandex.ru>
2023-05-06 20:50:13 +0300

    Serializers refactoring

--- events/serializers.py
+++ events/serializers.py
@@ -31,23 +31,24 @@ class EventRegistrationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventRegistrations
         fields = '__all__'
+        read_only_fields = ['shortuuid', ]
         
         
     def validate(self, attrs):
-        event = attrs["event_id"]
-        if event.closing_registration_date and event.closing_registration_date <= timezone.now():
+        event = attrs.get("event_id")
+        if event and event.closing_registration_date and event.closing_registration_date <= timezone.now():
             raise serializers.ValidationError({"closing_registration_date": "Нельзя зарегестрироваться после указанного времени закрытия регистрации"})
         return attrs
     
 
-class PrivateEventRegistrationsSerializer(serializers.Serializer):
+class PrivateEventRegistrationsSerializer(EventRegistrationsSerializer):
     class Meta:
         model = PrivateEventRegistrations
         fields = '__all__'
+        read_only_fields = ['shortuuid', ]
         
-        
-    def validate(self, attrs):
-        event = attrs["private_event_id"]
-        if event.closing_registration_date and event.closing_registration_date <= timezone.now():
-            raise serializers.ValidationError({"closing_registration_date": "Нельзя зарегестрироваться после указанного времени закрытия регистрации"})
-        return attrs
+
+class EventInvitationsSerializer(EventRegistrationsSerializer):
+    class Meta:
+            model = EventRegistrations
+            fields = ['user_id', ]
