dd454abf3f84dd23f89a08692b43d6aebd19c3ee
Re:Gelu <morandva@yandex.ru>
2023-05-06 20:48:30 +0300

    Added event invitation functionality

--- events/mixins.py
+++ events/mixins.py
@@ -1,8 +1,10 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from .serializers import EventInvitationsSerializer
+from .models import EventRegistrations, PrivateEventRegistrations
 
 class RegistrationModelMixin:
     """ Adds event registration functionality.
@@ -47,10 +49,68 @@ class InvitationModelMixin:
     event_registration_serializer_class = None
     event_registration_model = None
     
-    @action(detail=True, methods=['post'], serializer_class=None, permission_classes=[IsAuthenticated, ])
+    permission_classes=[IsAuthenticated, ]
+    
+    @action(detail=True, methods=['post'], serializer_class=EventInvitationsSerializer, permission_classes=permission_classes)
     def invitation(self, request, pk=None):
-        ...
+        """ Отправить приглашение на конкретное мероприятие пользователю или группе пользователей """
+        current_user = request.user
+        serializer = self.event_registration_serializer_class(data={
+            "event_id": pk,
+            "user_id": request.data.get("user_id"),
+            "is_invitation_accepted": False,
+            "inviting_user_id": current_user.id
+        })
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     @invitation.mapping.delete
     def delete_invitation(self, request, pk=None):
-        ...
+        """ Удалить приглашение на конкретное мероприятие пользователю или группе пользователей (Если он еще не принял приглашение) """
+        current_user = request.user
+        event_registration = get_object_or_404(
+            self.event_registration_model, 
+            event_id=pk, 
+            user_id=current_user.id,
+            is_invitation_accepted=False,
+        )
+        event_registration.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @action(detail=True, methods=['post'], serializer_class=None, permission_classes=[IsAuthenticated, ])
+    def confrim_invitation(self, request, pk=None):
+        """ Принять приглашение на конкретное мероприятие пользователю или группе пользователей """
+        current_user = request.user
+        instance = get_object_or_404(
+            self.event_registration_model, 
+            event_id=pk, 
+            user_id=current_user.id,
+            is_invitation_accepted=False,
+        )
+        serializer = self.event_registration_serializer_class(
+            instance,
+            data={ "is_invitation_accepted": True },
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'], permission_classes=permission_classes)
+    def guestlist(self, request, pk=None):
+        """ Получить список пользователей, принявших приглашение на конкретное мероприятие """
+        self.queryset = self.event_registration_model.objects.filter(event_id=pk, is_invitation_accepted=True)
+        self.serializer_class = self.event_registration_serializer_class
+        return self.list(self, request)
+    
+
+class PrivateInvitationModelMixin(InvitationModelMixin):
+    """ Adds private event invitation functionality.
+    
+    Required fields in ViewSet: 
+    1) event_registration_model
+    2) event_registration_serializer_class """
+    
+    permission_classes=[IsAdminUser, ]
--- events/views.py
+++ events/views.py
@@ -5,36 +5,20 @@ from rest_framework import viewsets, status
 from config.permissions import ReadOnly, ReadOnlyIfAuthenticated
 from .models import *
 from .serializers import *
-from .mixins import RegistrationModelMixin
+from .mixins import RegistrationModelMixin, InvitationModelMixin, PrivateInvitationModelMixin
 
 # ViewSets
 
-class EventsViewSet(viewsets.ModelViewSet, RegistrationModelMixin):
+class EventsViewSet(viewsets.ModelViewSet, RegistrationModelMixin, InvitationModelMixin):
     queryset = Events.objects.all()
     serializer_class = EventsSerializer
     permission_classes = [ReadOnly | IsAdminUser, ]
     
     event_registration_serializer_class = EventRegistrationsSerializer
     event_registration_model = EventRegistrations
-
-    @action(detail=True, methods=['post'], permission_classes=[IsAdminUser, ])
-    def invite(self, request, pk=None):
-        """ Отправить приглашение на конкретное мероприятие пользователю или группе пользователей """
-        return Response({})
-    
-    @action(detail=True, methods=['get'])
-    def invitations(self, request, pk=None):
-        """ Получить список приглашений на конкретное мероприятие """
-        return Response({})
-        
-    @action(detail=True, methods=['get'])
-    def guestlist(self, request, pk=None):
-        """ Получить список пользователей, принявших приглашение на конкретное мероприятие """
-        event = self.get_object()
-        return Response(event.visitors_list, status=status.HTTP_200_OK)
     
 
-class PrivateEventsViewSet(viewsets.ModelViewSet, RegistrationModelMixin):
+class PrivateEventsViewSet(viewsets.ModelViewSet, RegistrationModelMixin, PrivateInvitationModelMixin):
     queryset = PrivateEvents.objects.all()
     serializer_class = PrivateEventsSerializer
     permission_classes = [ReadOnlyIfAuthenticated | IsAdminUser, ]
