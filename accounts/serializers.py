d53a24c25f6dec1202ecaf8e611df662420cc191
Re:Gelu <morandva@yandex.ru>
2023-05-18 22:48:45 +0300

    Fixed read_only_fields in user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,12 +3,11 @@ from djoser import serializers as djoser_serializers
 from djoser.conf import settings
 from rest_framework import serializers
 
-from events.models import (EventRegistrations, Events, EventTypes, EventVenues,
-                     PaidEventRegistrations, PaidEvents,
-                     PrivateEventRegistrations, PrivateEvents)
-
-from events.serializers import (EventRegistrationsSerializer, PrivateEventRegistrationsSerializer,
-                                PaidEventRegistrationsSerializer)
+from events.models import (EventRegistrations, PaidEventRegistrations,
+                           PrivateEventRegistrations)
+from events.serializers import (EventRegistrationsSerializer,
+                                PaidEventRegistrationsSerializer,
+                                PrivateEventRegistrationsSerializer)
 
 
 class CustomUserSerializer(djoser_serializers.UserSerializer):
@@ -17,19 +16,19 @@ class CustomUserSerializer(djoser_serializers.UserSerializer):
     paid_event_registrations = serializers.SerializerMethodField()
     
     class Meta:
-        model = User
-        fields = tuple(User.REQUIRED_FIELDS) + (
-            settings.USER_ID_FIELD,
-            settings.LOGIN_FIELD,
-        ) + (
+        additional_fields = (
             'groups', 'is_staff', 
             'is_superuser', 'is_active',
             'first_name', 'last_name',
             'event_registrations', 'private_event_registrations',
             'paid_event_registrations'
 		)
-        read_only_fields = (settings.LOGIN_FIELD,'event_registrations', 
-                            'private_events_registrations', 'paid_events_registrations')
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            settings.USER_ID_FIELD,
+            settings.LOGIN_FIELD,
+        ) + additional_fields
+        read_only_fields = (settings.LOGIN_FIELD, ) + additional_fields
     
     def get_event_registrations(self, obj):
         queryset = EventRegistrations.objects.filter(
